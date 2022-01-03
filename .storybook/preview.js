e86fcc99199dda6e6bd15153836d531bf09012d9
Cyril Hue <cy.hue80@gmail.com>
2022-01-03 23:01:14 +0100

    ➕ add tailwindcss

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,4 @@
+import '../styles/global.css';
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
