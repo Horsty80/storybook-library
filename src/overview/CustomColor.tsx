7d7a0fec384d2979f991f18b1c1df243a245102a
Cyril Hue <cy.hue80@gmail.com>
2022-01-08 12:18:46 +0100

    💚 remove error import

--- src/overview/CustomColor.tsx
+++ src/overview/CustomColor.tsx
@@ -1,20 +1,19 @@
-import React from 'react';
-import '../../dist';
-import { ColorBox } from './ColorBox';
-import { KanomaTheme } from '../../models/theme';
+import React from "react";
+import { ColorBox } from "./ColorBox";
+import { KanomaTheme } from "../../models/theme";
 
 export interface Props {
-  color: 'kanoma-purple' | 'kanoma-yellow' | 'primary' | 'secondary';
+  color: "kanoma-purple" | "kanoma-yellow" | "primary" | "secondary";
   gradiant?:
-    | '100'
-    | '200'
-    | '300'
-    | '400'
-    | '500'
-    | '600'
-    | '700'
-    | '800'
-    | '900';
+    | "100"
+    | "200"
+    | "300"
+    | "400"
+    | "500"
+    | "600"
+    | "700"
+    | "800"
+    | "900";
 }
 
 export const CustomColor = ({ color, gradiant }: Props) => {
--- src/overview/ThemeIcon.tsx
+++ src/overview/ThemeIcon.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import resolveConfig from 'tailwindcss/resolveConfig';
-import tailwindConfig from '../../tailwind.config';
+import React from "react";
+import resolveConfig from "tailwindcss/resolveConfig";
+import tailwindConfig from "../../tailwind.config";
 
-import SvgUmbrella from '../../src/atoms/icons/SvgUmbrella';
-import SvgUsine from '../../src/atoms/icons/SvgUsine';
-import SvgValeurAjoutee from '../../src/atoms/icons/SvgValeurAjoutee';
-import SvgKanomaLogoFilaireBleu from '../../src/atoms/icons/SvgKanomaLogoFilaireBleu';
-import SvgPersoG from '../../src/atoms/icons/SvgPersoG';
+import SvgUmbrella from "../../src/atoms/icons/SvgUmbrella";
+import SvgUsine from "../../src/atoms/icons/SvgUsine";
+import SvgValeurAjoutee from "../../src/atoms/icons/SvgValeurAjoutee";
+import SvgKanomaLogoFilaireBleu from "../../src/atoms/icons/SvgKanomaLogoFilaireBleu";
+import SvgPersoG from "../../src/atoms/icons/SvgPersoG";
 
 const iconMap = {
   SvgUmbrella,
@@ -18,7 +17,7 @@ const iconMap = {
 };
 
 export interface Props {
-  color: 'primary' | 'secondary';
+  color: "primary" | "secondary";
 }
 
 const fullConfig = resolveConfig(tailwindConfig);
@@ -36,7 +35,7 @@ export const ThemeIcon = ({ color }: Props) => {
       {icons.map((Icon, i) => (
         <div key={i} className="p-3 flex flex-col justify-center space-y-3">
           <Icon
-            fill={color === 'primary' ? primary : secondary}
+            fill={color === "primary" ? primary : secondary}
             height={sizeLimit}
             width={sizeLimit}
           />
